5919ed416808deb2099a22a1309b8f3d4f541aa2
Igor Nagaev <blgarnag@gmail.com>
2022-04-03 18:13:37 +0300

    fix: fix logic validation form

--- react-components/src/pages/forms/Forms.tsx
+++ react-components/src/pages/forms/Forms.tsx
@@ -10,6 +10,28 @@ interface IUser {
   check: boolean;
 }
 
+interface IState {
+  users: IUser[];
+  nameError: string;
+  dateError: string;
+  cityError: string;
+  sexError: string;
+  photoError: string;
+  checkError: string;
+  formError: boolean;
+}
+
+const initialState: IState = {
+  users: [] as IUser[],
+  nameError: '',
+  dateError: '',
+  cityError: '',
+  sexError: '',
+  photoError: '',
+  checkError: '',
+  formError: true,
+};
+
 class Forms extends React.Component {
   nameRef: React.RefObject<HTMLInputElement> = React.createRef();
   dateRef: React.RefObject<HTMLInputElement> = React.createRef();
@@ -19,97 +41,155 @@ class Forms extends React.Component {
   photoRef: React.RefObject<HTMLInputElement> = React.createRef();
   checkRef: React.RefObject<HTMLInputElement> = React.createRef();
 
-  sex: string = this.menRef.current?.checked
-    ? 'men'
-    : this.womenRef.current?.checked
-    ? 'women'
-    : '';
-
-  state = {
-    users: [] as IUser[],
-    errors: {
-      name: 'введите ваше имя',
-      date: 'выберите дату поступления',
-      city: 'выберите ваш город',
-      sex: 'выберите ваш пол',
-      photo: 'загрузите ваше фото',
-      check: 'отметьте согласие',
-    },
+  getSex = () => {
+    return this.menRef.current?.checked ? 'men' : this.womenRef.current?.checked ? 'women' : '';
   };
 
-  handleName = () => {
+  state: IState = initialState;
+
+  handleNameChange = () => {
     if (this.nameRef.current?.value) {
-      this.setState({ errors: { ...this.state.errors, name: '' } });
+      this.setState({ nameError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, name: 'введите ваше ' } });
+      this.setState({ nameError: 'введите ваше ', formError: false });
     }
   };
 
-  handleDate = () => {
+  handleDateChange = () => {
     if (this.dateRef.current?.value) {
-      this.setState({ errors: { ...this.state.errors, date: '' } });
+      this.setState({ dateError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, date: 'выберите дату поступления' } });
+      this.setState({ dateError: 'выберите дату поступления', formError: false });
     }
   };
 
-  handleCity = () => {
+  handleCityChange = () => {
     if (this.cityRef.current?.value) {
-      this.setState({ errors: { ...this.state.errors, city: '' } });
+      this.setState({ cityError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, city: 'выберите ваш город' } });
+      this.setState({ cityError: 'выберите ваш город', formError: false });
     }
   };
 
-  handleSex = () => {
-    if (this.sex) {
-      this.setState({ errors: { ...this.state.errors, sex: '' } });
+  handleSexChange = () => {
+    const sex = this.getSex();
+    if (sex) {
+      this.setState({ sexError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, sex: 'выберите ваш пол' } });
+      this.setState({ sexError: 'выберите ваш пол', formError: false });
     }
   };
 
-  handlePhoto = () => {
+  handlePhotoChange = () => {
     if (this.photoRef.current?.files?.[0]) {
-      this.setState({ errors: { ...this.state.errors, photo: '' } });
+      this.setState({ photoError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, photo: 'загрузите ваше фото' } });
+      this.setState({ photoError: 'загрузите ваше фото', formError: false });
     }
   };
 
-  handleCheck = () => {
-    if (this.checkRef.current?.value) {
-      this.setState({ errors: { ...this.state.errors, check: '' } });
+  handleCheckChange = () => {
+    if (this.checkRef.current?.checked) {
+      this.setState({ checkError: '', formError: false });
     } else {
-      this.setState({ errors: { ...this.state.errors, check: 'отметьте согласие' } });
+      this.setState({ checkError: 'выберите ваш город', formError: false });
     }
+  };
+
+  validate = () => {
+    const sex = this.getSex();
+    let nameError = '',
+      dateError = '',
+      cityError = '',
+      sexError = '',
+      photoError = '',
+      checkError = '';
+
+    if (!this.nameRef.current?.value) {
+      nameError = 'введите ваше ';
+    }
+    if (!this.dateRef.current?.value) {
+      dateError = 'выберите дату поступления';
+    }
+    if (!this.cityRef.current?.value) {
+      cityError = 'выберите ваш город';
+    }
+    if (!sex) {
+      sexError = 'выберите ваш пол';
+    }
+    if (!this.photoRef.current?.files?.[0]) {
+      photoError = 'загрузите ваше фото';
+    }
+    if (!this.checkRef.current?.checked) {
+      checkError = 'отметьте согласие';
+    }
+
+    const formError = !(
+      !!this.nameRef.current?.value &&
+      !!this.dateRef.current?.value &&
+      !!this.cityRef.current?.value &&
+      (!!this.menRef.current?.checked || !!this.womenRef.current?.checked) &&
+      !!this.nameRef.current?.value &&
+      !!this.photoRef.current?.files?.[0] &&
+      !!this.checkRef.current?.checked
+    );
+
+    formError
+      ? this.setState({
+          nameError,
+          dateError,
+          cityError,
+          sexError,
+          photoError,
+          checkError,
+          formError,
+        })
+      : this.setState({
+          users: [
+            ...this.state.users,
+            {
+              name: this.nameRef.current?.value,
+              date: this.dateRef.current?.value,
+              city: this.cityRef.current?.value,
+              sex: sex,
+              photo: this.photoRef.current?.files?.[0],
+              check: this.checkRef.current?.checked,
+            },
+          ],
+          formError,
+        });
+
+    if (!formError) {
+      this.resetForm();
+    }
+  };
+
+  resetForm = () => {
+    if (this.nameRef.current?.value) {
+      this.nameRef.current.value = '';
+    }
+
+    this.dateRef.current!.value = '';
+    this.cityRef.current!.value = '';
+    this.menRef.current!.checked = false;
+    this.womenRef.current!.checked = false;
+    this.nameRef.current!.value = '';
+    this.photoRef.current!.files = null;
+    this.checkRef.current!.checked = false;
   };
 
   handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    this.setState({
-      users: [
-        ...this.state.users,
-        {
-          name: this.nameRef.current?.value,
-          date: this.dateRef.current?.value,
-          city: this.cityRef.current?.value,
-          sex: this.sex,
-          photo: this.photoRef.current?.files?.[0],
-          check: this.checkRef.current?.checked,
-        },
-      ],
-    });
+
+    this.validate();
   };
 
   render() {
-    const { name, check, city, date, photo, sex } = this.state.errors;
-    const formError = !!name || !!check || !!city || !!date || !!photo || !!sex;
-
+    console.log(this.state);
     return (
       <div className="forms">
-        <h2>Регистрация</h2>
         <form className="form" action="/" onSubmit={this.handleSubmit}>
+          <h2>Регистрация</h2>
           <div className="form__name">
             <label htmlFor="name">Ваше имя: </label>
             <input
@@ -117,19 +197,21 @@ class Forms extends React.Component {
               type="text"
               name="name"
               ref={this.nameRef}
-              onChange={this.handleName}
+              onChange={this.handleNameChange}
             />
           </div>
-          {this.state.errors.name && <div className="form__error">{this.state.errors.name}</div>}
+          {this.state.nameError && <div className="form__error">{this.state.nameError}</div>}
 
           <div className="form__date">
-            <label htmlFor="date">Дата вашего рождения: </label>
-            <input type="date" name="name" ref={this.dateRef} onChange={this.handleDate} />
+            <label htmlFor="date">Дата поступления: </label>
+            <input type="date" name="date" ref={this.dateRef} onChange={this.handleDateChange} />
+            {this.state.dateError && <div className="form__error">{this.state.dateError}</div>}
           </div>
 
           <div className="form__sity">
             <label htmlFor="city">Ваш город: </label>
-            <select id="city" name="city" ref={this.cityRef} onChange={this.handleCity}>
+            <select id="city" name="city" ref={this.cityRef} onChange={this.handleCityChange}>
+              <option></option>
               <option value="Минск">Минск</option>
               <option value="Гомель">Гомель</option>
               <option value="Брест">Брест</option>
@@ -137,12 +219,19 @@ class Forms extends React.Component {
               <option value="Могилев">Могилев</option>
               <option value="Витебск">Витебск</option>
             </select>
+            {this.state.cityError && <div className="form__error">{this.state.cityError}</div>}
           </div>
 
           <div className="form__sex">
             Ваш пол:
             <div>
-              <input id="men" type="radio" name="sex" ref={this.menRef} onChange={this.handleSex} />
+              <input
+                id="men"
+                type="radio"
+                name="sex"
+                ref={this.menRef}
+                onChange={this.handleSexChange}
+              />
               <label htmlFor="men">Мужской</label>
             </div>
             <div>
@@ -151,31 +240,37 @@ class Forms extends React.Component {
                 type="radio"
                 name="sex"
                 ref={this.womenRef}
-                onChange={this.handleSex}
+                onChange={this.handleSexChange}
               />
               <label htmlFor="women">Женский</label>
             </div>
+            {this.state.sexError && <div className="form__error">{this.state.sexError}</div>}
           </div>
 
-          <input type="file" id="photo" ref={this.photoRef} onChange={this.handlePhoto} />
+          <input type="file" id="photo" ref={this.photoRef} onChange={this.handlePhotoChange} />
           <label htmlFor="photo">Выбрать фото</label>
+          {this.state.photoError && <div className="form__error">{this.state.photoError}</div>}
+
           <div className="form__check">
             <input
               type="checkbox"
               id="check"
               name="check"
               ref={this.checkRef}
-              onChange={this.handleCheck}
+              onChange={this.handleCheckChange}
             />
             <label htmlFor="check">Согласен на обработку данных</label>
+            {this.state.checkError && <div className="form__error">{this.state.checkError}</div>}
           </div>
-          <button type="submit">Зарегистрироваться</button>
+          <button type="submit" disabled={this.state.formError}>
+            Зарегистрироваться
+          </button>
         </form>
 
         <div className="users">
           {this.state.users.length
             ? this.state.users.map((el, ind) => {
-                const url = URL.createObjectURL(el.photo);
+                const url = URL.createObjectURL(el?.photo);
                 return (
                   <div key={ind} className="user">
                     <div className="user__photo">
